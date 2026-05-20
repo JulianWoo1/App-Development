270a39c983d8bd332a8c429b4ca2a0722dd75c25
JoranVos <joran.vos@student.nhlstenden.com>
2026-05-20 12:37:38 +0200

    Fix NoInternetScreen Compose layout and enable proper display on connectivity changes

--- app/src/main/java/com/example/realitycheck/MainActivity.kt
+++ app/src/main/java/com/example/realitycheck/MainActivity.kt
@@ -3,15 +3,47 @@ package com.example.realitycheck
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.runtime.*
 import com.example.realitycheck.ui.MainNavHost
+import com.example.realitycheck.ui.nointernet.NoInternetScreen
 import com.example.realitycheck.ui.theme.RealityCheckTheme
+import com.example.realitycheck.utils.NetworkMonitor
 
-class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+class MainActivity : ComponentActivity()
+{
+    private lateinit var networkMonitor: NetworkMonitor
+
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
+
         setContent {
             RealityCheckTheme {
-                MainNavHost()
+
+                var isOnline by remember { mutableStateOf(true) }
+
+                networkMonitor = NetworkMonitor(this) { connected ->
+                    isOnline = connected
+                }
+
+                LaunchedEffect(Unit) {
+                    networkMonitor.startMonitoring()
+                }
+
+                DisposableEffect(Unit) {
+                    onDispose {
+                        networkMonitor.stopMonitoring()
+                    }
+                }
+
+                if (!isOnline)
+                {
+                    NoInternetScreen( onRetrySuccess = { })
+                }
+                else
+                {
+                    MainNavHost()
+                }
             }
         }
     }
